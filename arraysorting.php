84d0affcc37d418df0106a6e2e9e194ee94b6d53
Clement Cabus <chummycabus@gmail.com>
2023-04-23 10:40:25 +0800

    php

--- arraysorting.php
+++ arraysorting.php
@@ -23,7 +23,15 @@
                            */
 
             //sort()
-        
+            $cars = array("Volvo", "BMW", "Toyota");
+            sort($cars);
+            $clength = count($cars);
+	        for($x = 0; $x < $clength; $x++) {
+ 	        echo $cars[$x];
+	        echo "<br>";
+            }
+
+            //asort()
         
         ?>
     </form>
